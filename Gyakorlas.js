3b02ce834a021446d5488808466af7e4cd854488
Tóth Gergő <36165484+TothGerg0@users.noreply.github.com>
2018-09-23 16:34:24 +0200

    Update Gyakorlas.js

--- Gyakorlas.js
+++ Gyakorlas.js
@@ -9,14 +9,14 @@ greatBalrogOfMoria: {hp: 0, hpMax: 34, mp: 0, mpMax: 20, xp:0, exist: "false"
 }
 };
 
-let varazslatok = { slash: { damage: 1, mpCost: 0, effect:"", caster:"g"}, 
-                    smite: { damage: 4, mpCost: 3, effect:"", caster:"g" }, 
-                    heal: { damage:0 , mpCost:3 , effect:"heal4", caster:"g" },
-                    flameOfAnor { damage: 20, mpCost: 15, effect:"blind1", caster:"gg" }, 
-                    flameWhip: { damage: 1, mpCost: 0, effect:"", caster:"b" }, 
-                    burningBlade: { damage: 3, mpCost: 2, effect:"", caster:"b" },
-                    manaDrain: { damage: 0, mpCost: 3, effect:"drain2", caster:"b" }, 
-                    shadowOfMorgoth: { damage: 17, mpCost: 11, effect:"blind1", caster:"bb" }
+let varazslatok = { slash:           { damage: 1,   mpCost: 0,  effect:"",        caster:"g"}, 
+                    smite:           { damage: 4,   mpCost: 3,  effect:"",        caster:"g" }, 
+                    heal:            { damage: 0,   mpCost:3 ,  effect:"heal4",   caster:"g" },
+                    flameOfAnor      { damage: 20,  mpCost: 15, effect:"blind1",  caster:"gg" }, 
+                    flameWhip:       { damage: 1,   mpCost: 0,  effect:"",        caster:"b" }, 
+                    burningBlade:    { damage: 3,   mpCost: 2,  effect:"",        caster:"b" },
+                    manaDrain:       { damage: 0,   mpCost: 3,  effect:"drain2",  caster:"b" }, 
+                    shadowOfMorgoth: { damage: 17,  mpCost: 11, effect:"blind1",  caster:"bb" }
 };
 for (i=0; i < 10; i++){
   
