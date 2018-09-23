e04a76bedbe5a55405c493d496f6462927b71709
Tóth Gergő <36165484+TothGerg0@users.noreply.github.com>
2018-09-23 15:15:46 +0200

    Update Gyakorlas.js

--- Gyakorlas.js
+++ Gyakorlas.js
@@ -1,8 +1,8 @@
-let Varazslok = { gandalfTheGrey: { hp: 0, mp: 0, xp:0,
+let Varazslok = { gandalfTheGrey: { hp: 10, mp: 17, xp:0,
 },
 gandalfTheWhite: {hp: 0, mp: 0, xp:0,
 },
-balrogOfMoria: {hp: 0, mp: 0, xp:0,
+balrogOfMoria: {hp: 17, mp: 10, xp:0,
 },
 greatBalrogOfMoria: {hp: 0, mp: 0, xp:0,
 }
