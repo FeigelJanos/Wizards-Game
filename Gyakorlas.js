2e1978c9d16ad876f16608e57ac1f817801e7d34
FeigelJanos <32643682+FeigelJanos@users.noreply.github.com>
2018-09-23 15:36:25 +0200

    Update Gyakorlas.js

--- Gyakorlas.js
+++ Gyakorlas.js
@@ -1,10 +1,10 @@
-let Varazslok = { gandalfTheGrey: { hp: 10, mp: 17, xp:0,
+let Varazslok = { gandalfTheGrey: { hp: 10, hpMax: 10, mp: 17, mpMax: 17, xp:0,
 },
-gandalfTheWhite: {hp: 0, mp: 0, xp:0,
+gandalfTheWhite: {hp: 0, hpMax: 20,  mp: 0, mpMax: 34, xp:0,
 },
-balrogOfMoria: {hp: 17, mp: 10, xp:0,
+balrogOfMoria: {hp: 17, hpMax: 17, mp: 10, mpMax: 10, xp:0,
 },
-greatBalrogOfMoria: {hp: 0, mp: 0, xp:0,
+greatBalrogOfMoria: {hp: 0, hpMax: 34, mp: 0, mpMax: 20, xp:0,
 }
 };
 
